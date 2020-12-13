738145b1a4093aea615e9136bb0d3f9e5ede5781
Joshua Vedane <jvedane@gmail.com>
2020-12-13 13:27:57 -0600

    completed base mode. Styling added, comments removed from client.js, README updated.

--- JS/client.js
+++ JS/client.js
@@ -1,4 +1,4 @@
-console.log('JS');
+// console.log('JS');
 
 //array to hold input field info
 let employees = [];
@@ -8,19 +8,13 @@ let monthlyCost = 0;
 
 $(document).ready(handleReady);
 
-
-//submitbuttonOnClick function will take the values of the input fields and push them into the global empty array. It will also call the renderToDom function to get those values displayed. Also, let's clear the input fields after submission so we can keep rolling. 
-
-//create an renderToDom function that is called in handleReady AND also on submit button click. This way it will update on page load and also every time button clicked. First, clear out that empty array, NO DUPLICATES TODAY! Second, we want to loop over the employees array. For each object in the array create a variable to hold the info. We want to create a new table row with each key:value pair taking up a single <td>. Then simply append that variable to the tbody "employeeListOut". maybe call calculateTotalMonthly at the end of this? we'll see. 
-
-//calculateTotalMonthly function is where the real work is. We need to loop through all of the values of the employee array and grab each annual salary. For each salary, we need to divide it by 12 (months) and either round that number to the nearest cent or something similar. Then, we need to knock that number into a variable that will then be rendered to the DOM. don't forget, annualSalary is a string that needs to get converted to a number!!!!!!
-
 function handleReady() {
-    console.log('JQ');
+    // console.log('JQ');
     renderToDom();
-    
+
     //click listeners
     $('#addEmployee').on('click', handleAddEmployeeClick);
+    $('#employeeListOut').on('click', '.delete', handleDeleteEmployeeClick);
 }
 
 //renderToDom
@@ -30,68 +24,76 @@ function renderToDom() {
     //input field rendering
     for (let employee of employees) {
         //create the employee table row
+        let employeeSalaryNum = Number(employee.annualSalary);
+        let employeeSalaryOut = employeeSalaryNum.toLocaleString();
         let employeeRow = $(`<tr>
         <td>${employee.firstName}</td>
         <td>${employee.lastName}</td>
         <td>${employee.id}</td>
         <td>${employee.title}</td>
-        <td>$${employee.annualSalary}</td>
+        <td>$${employeeSalaryOut}</td>
+        <td><button class = "btn btn-dark delete">Delete</button></td>
         </tr>`);
 
         //append employee info to the DOM
         $('#employeeListOut').append(employeeRow);
-        console.log('inRenderToDom');
+        // console.log('inRenderToDom');
 
         //calculate monthlyCost
-        monthlyCost = annualTotalCost/12;
+        monthlyCost = annualTotalCost / 12;
         //check if over budget
-        checkOverBudget();
+        checkOverBudget(monthlyCost);
         //format monthlyCost for output.
         let roundMonthlyCost = Math.round(monthlyCost);
         let monthlyCostOut = roundMonthlyCost.toLocaleString();
-        console.log(`this should be monthly cost: ` ,monthlyCostOut);
+        // console.log(`this should be monthly cost: `, monthlyCostOut);
         //render Monthly Cost to DOM
         $('.totalOut').text(monthlyCostOut);
     }
 }
 
 //check if over budget
-function checkOverBudget(){
-    if(monthlyCost >= 20000){
-        $('.totalOut').toggleClass('over-budget');
+function checkOverBudget(monthlyCost) {
+    if (monthlyCost >= 20000) {
+        $('.totalOut').addClass('over-budget');
     }
 }
 
 
 //Submit button handler
-function handleAddEmployeeClick(){
-    console.log('button click');
+function handleAddEmployeeClick() {
+    // console.log('button click');
     let newEmployee = {
-        firstName : $('#firstNameIn').val(),
-        lastName : $('#lastNameIn').val(),
-        id : $('#idIn').val(),
-        title : $('#titleIn').val(),
-        annualSalary : $('#annualSalaryIn').val()
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        id: $('#idIn').val(),
+        title: $('#titleIn').val(),
+        annualSalary: $('#annualSalaryIn').val()
     };
     //store the annual salary of each employee in variable for use in calculating total monthly cost later. 
     annualTotalCost += Number(newEmployee.annualSalary);
-    console.log(`testing:` , parseInt(annualTotalCost));
+    // console.log(`testing:`, parseInt(annualTotalCost));
 
     //make sure we get the right stuff
-    console.log(newEmployee);
+    // console.log(newEmployee);
     //newEmployee now in employees array. 
     employees.push(newEmployee);
     //make sure it pushed newEmployee to array
-    console.log(employees);
+    // console.log(employees);
 
-    //call renderToDom. When we click on submit, it will push a new object into employees array. We then want it to show up the DOM. see zhu-bootstrap for that. 
+    //call renderToDom. When we click on submit, it will push a new object into employees array. We then want it to show up the DOM.
     renderToDom();
 
     //we also want to clear the input fields. Let's make a new function that does that and it will be called here. 
     clearFields();
 }
 
-function clearFields(){
+function handleDeleteEmployeeClick() {
+    console.log('delete works!');
+    $(this).parent().parent().remove();
+}
+
+function clearFields() {
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idIn').val('');
--- README.md
+++ README.md
@@ -1,9 +1,49 @@
 # Project Name
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+Monthly Salary Calculator
 
 ## Description
 
 Your project description goes here. What problem did you solve? How did you solve it?
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+This is a monthly salary application that keeps track of the monthly salaries of employees. The user adds an employee's information into the text fields and clicks the submit button. This displays the information in the table below. The annual salaries of all the employees added are calculated and converted to a total monthly cost which is displayed under the table. 
+
+Once the monthly total cost for all employees added hits a certain number, in this case, $20,000, the total monthly cost will turn red. This shows that the payroll is over budget. 
+
+If an employee was added by mistake or there was a typing error on input, there is a delete button that will remove that employee from the table. (This does not change the monthly cost displayed). 
+
+## In Depth Explanation 
+
+This project is a simple webpage that runs all of the calculations in javascript. To recreate this project, follow these steps
+1. Create HTML, CSS, JS files and bring BOOTSTRAP and JQUERY files into the directory. Link/source all files. 
+2. In HTML, create: 
+    a. Header
+    b. Input field
+    c. 'Submit' button 
+    d. table with table head
+    e. Section for displaying monthly costs.
+3. Style to suit. This project combined BOOTSTRAP with some customized styling in CSS file.
+4.  In JS : 
+    a. create three global variables to hold 1(empty array) 2(total employee annual salaries) 3(total monthly cost)
+    b. call a JQ function to handle document ready
+    c. create document ready function that: 
+        i. calls a function that will render to the DOM the information added to input fields. 
+        ii. add two click listeners that listen for the submit button click and the delete button click(explained below).
+    d. create the submit button handler. This will create a new object for all the information added to the input fields upon click. Take the annual salary of each employee and add it the total annual cost global variable. Next, push this newly created object to the empty employees array. Call a function that will handle rendering this information to the DOM. Also, call a function that resets the input fields to empty after button click. 
+    e. create a function that handles rendering information to the DOM. This will: 
+        i. clear out the tbody so no duplications will be rendered. 
+        ii. loop through the employees array and append all information to the table body in their respective table columns. 
+        iii. calculate the monthlyCost by dividing the total annual cost by 12. 
+        iv. call a budget checker function. 
+            a. This function will check if the monthly cost is greater than a set amount. If it is, add a class to the monthly cost display element which changes the background color of that element. 
+        v. render the monthly cost to the DOM. 
+        vi. create the delete button handler 
+            i. this button will delete the current table row
+
+
+## Usage 
+1. open the html file in browser
+2. input employee information into their respective input fields
+3. click the submit button 
+4. repeat steps 2&3 for all employees
+5. check the total monthly cost at the bottom of the page if under/over budget
